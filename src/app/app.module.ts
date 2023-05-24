63cc9079ed291b3392950e424c92cec7d36e0d8e
Jose Diaz <84774786+diazjdev@users.noreply.github.com>
2023-05-24 22:52:10 +0100

    feat(shell): create shell module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,37 +1,12 @@
-import { NgModule, isDevMode } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { StoreModule } from '@ngrx/store';
-import { EffectsModule } from '@ngrx/effects';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { AuthFeatureModule } from 'auth/feature';
-import { IAuthConfig, oktaAuthConfig } from 'auth/data-access';
-
-const config: IAuthConfig = {
-  issuer: 'https://dev-55604879.okta.com/oauth2/default',
-  clientId: '0oa9n150r05tJiqt05d7',
-  redirectUri: window.location.origin + '/login/callback',
-};
-const oktaAuth = oktaAuthConfig(config);
+import { ShellFeatureModule } from 'shell/feature';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [
-    BrowserModule,
-    StoreModule.forRoot({}),
-    EffectsModule.forRoot([]),
-    AuthFeatureModule.forRoot(oktaAuth),
-    StoreDevtoolsModule.instrument({
-      maxAge: 25, // Retains last 25 states
-      logOnly: !isDevMode(), // Restrict extension to log-only mode
-      autoPause: true, // Pauses recording actions and state changes when the extension window is not open
-      trace: false, //  If set to true, will include stack trace for every dispatched action, so you can see it in trace tab jumping directly to that part of code
-      traceLimit: 75, // maximum stack trace frames to be stored (in case trace option was provided as true)
-    }),
-    AppRoutingModule,
-  ],
+  imports: [BrowserModule, ShellFeatureModule],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
