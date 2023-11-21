c490135a8032cf1ee471f0b175abeec8bafb110e
@diazjdev <joseinyork+git@gmail.com>
2023-11-21 21:15:38 +0000

    feat(cart): add feature store

--- libs/cart/data-access/src/index.ts
+++ libs/cart/data-access/src/index.ts
@@ -1,1 +1,6 @@
+export * from './lib/+state/cart.facade';
+export * from './lib/+state/cart.models';
+export * from './lib/+state/cart.selectors';
+export * from './lib/+state/cart.reducer';
+export * from './lib/+state/cart.actions';
 export * from './lib/cart-data-access.module';
